f2c5a170f5f01788bcf3c40d777c467bfd9adb14
Matt Gibson <gibson.matthew@hotmail.com>
2023-07-20 20:01:43 -0400

    Add Shared Class Library Project for Models

---
+++ EstiTrack.Shared/Models/Estimate.cs
@@ -0,0 +1,10 @@
+namespace EstiTrack.Shared.Models;
+
+public class Estimate
+{
+    public Guid Id { get; set; }
+    public DateOnly DateCreated { get; set; }
+    public DateOnly DateUpdated { get; set; }
+    public Customer Customer { get; set; } = new();
+    public bool IsRush { get; set; }
+}
