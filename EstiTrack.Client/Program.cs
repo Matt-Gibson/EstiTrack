d7963a04f3d81b211f7e4d9013456e306ab1ff79
Matt-Gibson PVWD <gibson.matthew@hotmail.com>
2023-07-21 14:13:12 -0400

    Add Sumbission form and /estimate component

--- EstiTrack.Client/Program.cs
+++ EstiTrack.Client/Program.cs
@@ -8,6 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IEstimateService, EstimateService>();
+//builder.Services.AddScoped<IEstimateService, EstimateService>();
 
 await builder.Build().RunAsync();
--- EstiTrack.Client/Services/EstimateService/EstimateService.cs
+++ EstiTrack.Client/Services/EstimateService/EstimateService.cs
@@ -1,6 +1,35 @@
 namespace EstiTrack.Client.Services.EstimateService;
+using EstiTrack.Shared.Models;
 
-public class EstimateService : IEstimateService
+public static class EstimateService
 {
+     private static readonly List<Estimate> estimates = new()
+    {
+        new Estimate()
+        {
+            Id = 1,
+            Customer = new(),
+            JobType = "Gutter",
+            IsRush = true
+        },
+        new Estimate()
+        {
+            Id = 2,
+            Customer = new(),
+            JobType = "Window",
+            IsRush = false
+        },
+        new Estimate()
+        {
+            Id = 3,
+            Customer = new(),
+            JobType = "Roof",
+            IsRush = true
+        }
+    };
 
+    public static Estimate[] GetEstimates()
+    {
+        return estimates.ToArray();
+    }
 }
