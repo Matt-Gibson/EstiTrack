8e132b99e6cbde883c087aff1122b439f08a167b
Matt Gibson <gibson.matthew@hotmail.com>
2023-07-21 20:18:02 -0400

    Validator change

--- EstiTrack.Shared/Models/Customer.cs
+++ EstiTrack.Shared/Models/Customer.cs
@@ -6,10 +6,18 @@ public class Customer
     public int Id { get; set; }
 
     [Required]
+    [StringLength(20)]
+    [MinLength(2)]
     public string FirstName { get; set; } = string.Empty;
+
     [Required]
+    [StringLength(20)]
+    [MinLength(2)]
     public string LastName { get; set; } = string.Empty;
+
     [Required]
+    [StringLength(20)]
     public string PhoneNumber { get; set; } = string.Empty;
+
     public bool Active { get; set; }
 }
--- EstiTrack.Shared/Models/Estimate.cs
+++ EstiTrack.Shared/Models/Estimate.cs
@@ -4,11 +4,30 @@ namespace EstiTrack.Shared.Models;
 public class Estimate
 {
     public int Id { get; set; }
-    public DateOnly DateCreated { get; set; }
-    public DateOnly DateUpdated { get; set; }
-    public required Customer Customer { get; set; } = new();
 
     [Required]
+    public DateOnly DateCreated { get; set; }
+
+    [Required]
+    public DateOnly DateUpdated { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required String JobType { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    [MinLength(2)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(20)]
+    [MinLength(2)]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(20)]
+    public string PhoneNumber { get; set; } = string.Empty;
+
     public required bool IsRush { get; set; }
 }
