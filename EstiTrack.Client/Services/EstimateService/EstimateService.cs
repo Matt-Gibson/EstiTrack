f44846c4642919114811749a994b545db07123ec
Matt-Gibson PVWD <gibson.matthew@hotmail.com>
2023-07-24 14:52:22 -0400

    Add Edit and Delete Button functionality with pop-up modal

--- EstiTrack.Client/Services/EstimateService/EstimateService.cs
+++ EstiTrack.Client/Services/EstimateService/EstimateService.cs
@@ -1,4 +1,7 @@
 namespace EstiTrack.Client.Services.EstimateService;
+
+using System.ComponentModel;
+
 using EstiTrack.Shared.Models;
 
 public static class EstimateService
@@ -7,19 +10,34 @@ public static class EstimateService
     {
         new Estimate()
         {
+            FirstName = "Steve",
+            LastName = "Caudill",
+            PhoneNumber = "555-555-4782",
             Id = 1,
+            DateCreated = new DateOnly(2022,01,02),
+            DateUpdated = new DateOnly(2023,05,06),
             JobType = "Gutter",
             IsRush = true
         },
         new Estimate()
         {
+            FirstName = "Justin",
+            LastName = "Cox",
+            PhoneNumber = "222-544-4588",
             Id = 2,
+            DateCreated = new DateOnly(2021,10,12),
+            DateUpdated = new DateOnly(2022,07,04),
             JobType = "Window",
             IsRush = false
         },
         new Estimate()
         {
+            FirstName = "Bob-da",
+            LastName = "Butcher",
+            PhoneNumber = "859-867-5309",
             Id = 3,
+            DateCreated = new DateOnly(2000,10,09),
+            DateUpdated = new DateOnly(2021,08,11),
             JobType = "Roof",
             IsRush = true
         }
@@ -51,5 +69,11 @@ public static class EstimateService
         existingEstimate.IsRush = updatedEstimate.IsRush;
         existingEstimate.DateCreated = updatedEstimate.DateCreated;
         existingEstimate.DateUpdated = updatedEstimate.DateUpdated;
+    }
+
+    public static void DeleteEstimate(int id)
+    {
+        Estimate estimate = GetEstimateByID(id);
+        estimates.Remove(estimate);
     }
 }
