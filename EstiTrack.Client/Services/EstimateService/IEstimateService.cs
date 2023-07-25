7863b9996bd80175e68e373d1ae3bd5f1bc8c0df
Matt Gibson <gibson.matthew@hotmail.com>
2023-07-25 18:18:47 -0400

    Began Interface

--- EstiTrack.Client/Services/EstimateService/IEstimateService.cs
+++ EstiTrack.Client/Services/EstimateService/IEstimateService.cs
@@ -1,6 +1,10 @@
 namespace EstiTrack.Client.Services.EstimateService;
+using EstiTrack.Shared.Models;
 
     public interface IEstimateService
     {
-        
+        List<Estimate> Estimates {get; set;}
+
+        Task GetEstimates();
+        Task<Estimate?> GetEstimateById(int id);
     }
