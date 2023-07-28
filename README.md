efe8de5bead018f8ba39e08850f4b85da999defa
Matt Gibson <gibson.matthew@hotmail.com>
2023-07-27 22:26:07 -0400

    Redone as a Web Api backend and WASM Front

--- README.md
+++
@@ -1,1 +0,0 @@
-# EstiTrack
---
+++ Server/Program.cs
@@ -0,0 +1,43 @@
+﻿using EstimateTracker.Data.Interfaces;
+using EstimateTracker.Data.Services;
+using EstimateTracter.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddDbContextFactory<EstimateDbContext>(opt=>opt.UseSqlite($"DataSource=../myapp.db"));
+builder.Services.AddScoped<IEstimateService, EstimateService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
+
